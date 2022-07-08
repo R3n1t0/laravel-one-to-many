6ff1f0e01c82fb95553ae7d93b9ba96f05e7455a
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-07-08 18:02:08 +0200

    Creo model, tabella con le varie colonne e il seeder con i dati da popolare

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+Use App\Category;
+use Illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'HTML',
+            'PHP',
+            'CSS',
+            'JavaScript',
+            'Python',
+            'C++',
+            'VueJs',
+            'Laravel'
+        ];
+
+        foreach($categories as $category){
+
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($category, '-');
+            $new_category->save();
+
+        }
+    }
+}
